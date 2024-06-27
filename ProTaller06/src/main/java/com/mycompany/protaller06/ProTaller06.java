d522e2e5e91d385514cab7f3f39b428e8eec4273
Jonthz <jnzambra@fiec.edu.espol.ec>
2024-06-26 22:31:01 -0500

    singleton desing pattern added

--- ProTaller06/src/main/java/com/mycompany/protaller06/ProTaller06.java
+++ ProTaller06/src/main/java/com/mycompany/protaller06/ProTaller06.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.protaller06;
 
+import singleton.ConfiguracionJuego;
+
 /**
  *
  * @author CltControl
@@ -12,5 +14,25 @@ public class ProTaller06 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        // Codigo para uso del patron Singleton en el main
+            ConfiguracionJuego config = ConfiguracionJuego.getInstancia();
+
+            // Configurar el juego según la configuración
+            System.out.println("Dimensiones de pantalla: " + config.getDimensionesPantalla());
+            System.out.println("Memoria RAM mínima: " + config.getMemoriaRAMMinima() + " MB");
+            System.out.println("Espacio en disco mínimo: " + config.getEspacioDiscoMinimo() + " MB");
+
+            // Ejemplo de cambiar la configuración
+            config.setDimensionesPantalla("2560x1440");
+            config.setMemoriaRAMMinima(4096);
+            config.setEspacioDiscoMinimo(20480);
+
+            // Mostrar la nueva configuración
+            System.out.println("Nueva configuración:");
+            System.out.println("Dimensiones de pantalla: " + config.getDimensionesPantalla());
+            System.out.println("Memoria RAM mínima: " + config.getMemoriaRAMMinima() + " MB");
+            System.out.println("Espacio en disco mínimo: " + config.getEspacioDiscoMinimo() + " MB");
+        
     }
 }
